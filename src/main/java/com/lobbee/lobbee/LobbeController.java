2e75877553bfe0a2b6c656205125084cefd02ba7
niromon <morin.arnaud@gmail.com>
2017-09-30 21:59:30 +0200

    fist commit

---
+++ src/main/java/com/lobbee/lobbee/LobbeController.java
@@ -0,0 +1,11 @@
+package com.lobbee.lobbee;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+public class LobbeController {
+}
---
+++ src/main/java/com/lobbee/lobbee/LobbeeApplication.java
@@ -0,0 +1,12 @@
+package com.lobbee.lobbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbeeApplication.class, args);
+	}
+}
